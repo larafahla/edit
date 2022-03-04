184b1214362de64d6dd6166be569beb9f12379d7
Lara Fahla <larasantosfahla@gmail.com>
2022-03-04 22:50:51 +0000

    commit

--- REACT/project/src/index.js
+++ REACT/project/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Context, createStore } from './services/Store'
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
